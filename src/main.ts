41d6ad86a3a25c172b588b54170b9fb7b527b868
skycader <skycader@yandex.ru>
2024-06-20 01:14:42 +0300

    main: better dto

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { json, urlencoded } from 'express';
-import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
+import { NestFactory } from "@nestjs/core";
+import { json, urlencoded } from "express";
+import { AppModule } from "./app.module";
+import * as bodyParser from "body-parser";
+import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(json({ limit: '5mb' }));
-  app.use(urlencoded({ extended: true, limit: '5mb' }));
+  app.use(json({ limit: "5mb" }));
+  app.use(urlencoded({ extended: true, limit: "5mb" }));
   app.use(
     bodyParser.urlencoded({
-      limit: '5mb',
+      limit: "5mb",
       extended: true,
       parameterLimit: 5000000,
-    }),
+    })
   );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    })
+  );
+
   await app.listen(3000);
 }
 bootstrap();
