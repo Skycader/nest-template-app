8a5cae82423b37539dda1e6564d31e5fb38e0635
skycader <skycader@yandex.ru>
2024-06-24 13:24:46 +0300

    main: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,37 @@
-import { NestFactory } from "@nestjs/core";
-import { json, urlencoded } from "express";
-import { AppModule } from "./app.module";
-import * as bodyParser from "body-parser";
-import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
+import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(json({ limit: "5mb" }));
-  app.use(urlencoded({ extended: true, limit: "5mb" }));
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
   app.use(
     bodyParser.urlencoded({
-      limit: "5mb",
+      limit: '5mb',
       extended: true,
       parameterLimit: 5000000,
-    })
+    }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       forbidUnknownValues: true,
-    })
+    }),
   );
 
   await app.listen(3000);
