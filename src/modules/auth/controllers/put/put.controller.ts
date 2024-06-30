80741664ab439e675f163ca053d23db1952b831c
skycader <skycader@yandex.ru>
2024-06-30 15:49:51 +0300

    main: admin cannot self kill

--- src/modules/auth/controllers/put/put.controller.ts
+++ src/modules/auth/controllers/put/put.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Param,
@@ -6,22 +7,24 @@ import {
   Put,
   UseGuards,
   ValidationPipe,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AuthService } from '../../services/auth.service';
-import { IsAdminGuard } from '../../guards/is-admin.guard';
-import { UpdateRoleDto } from '../../dtos/update-role.dto';
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { AuthService } from "../../services/auth.service";
+import { IsAdminGuard } from "../../guards/is-admin.guard";
+import { UpdateRoleDto } from "../../dtos/update-role.dto";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthPutController {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-  @Put('/update-role/:username')
+  @Put("/update-role/:username")
   @UseGuards(AuthGuard(), IsAdminGuard)
   public async patchRole(
-    @Param('username') username: string,
-    @Body(ValidationPipe) patch: UpdateRoleDto,
+    @Param("username") username: string,
+    @Body(ValidationPipe) patch: UpdateRoleDto
   ) {
+    if (username === "admin")
+      throw new BadRequestException("Admin cannot self kill");
     const status = await this.authService.updateRole(username, patch.role);
     return { status };
   }
